35929c69359dae225752a56e03496bf159236e16
Alejandro Fernández de Larrea <alefdzknoflach@gmail.com>
2024-11-21 17:03:19 +0100

    Enemy Waves and Exp
    -Enemies now spawn in waves
    -Set up exp system

--- Assets/Scripts/Game/Enemyspawner.cs
+++ Assets/Scripts/Game/Enemyspawner.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 public class Enemyspawner : MonoBehaviour
 {
     public GameObject bananaSpiderPrefab;
-    public GameObject steakSlugPrefab;
 
     public float timeToSpawn; //Sets the timer
     private float spawnCounter; //IS the timer
@@ -19,25 +18,42 @@ public class Enemyspawner : MonoBehaviour
     public int checkPerFrame;
     private int enemyToCheck;
 
+    public List<WaveInfo> waves;
+
+    private int currentWave;
+
+    private float waveCounter;
+
     private void Start()
     {
-        spawnCounter = timeToSpawn;
-
         despawnDistance = Vector3.Distance(transform.position, maxSpawn.position) + 4f;
+
+        currentWave = -1;
+        GoToNextWave();
     }
 
     void Update()
     {
-        //ticks down per second
-        spawnCounter -= Time.deltaTime;
-
-        if(spawnCounter <= 0)
+        if (PlayerHealthController.instance.gameObject.activeSelf)
         {
-            spawnCounter = timeToSpawn;
+            if (currentWave < waves.Count)
+            {
+                waveCounter -= Time.deltaTime;
+                if (waveCounter <= 0)
+                {
+                    GoToNextWave();
+                }
 
-            GameObject newEnemy = Instantiate(bananaSpiderPrefab, SelectSpawnpoint(), transform.rotation);
+                spawnCounter -= Time.deltaTime;
+                if (spawnCounter <= 0)
+                {
+                    spawnCounter = waves[currentWave].timeBetweenSpawns;
 
-            spawnedEnemies.Add(newEnemy);
+                    GameObject newEnemy = Instantiate(waves[currentWave].enemyToSpawn, SelectSpawnpoint(), Quaternion.identity);
+
+                    spawnedEnemies.Add(newEnemy);
+                }
+            }
         }
 
         int checkTarget = enemyToCheck + checkPerFrame;
@@ -112,4 +128,25 @@ public class Enemyspawner : MonoBehaviour
 
         return spawnPoint;
     }
+
+    public void GoToNextWave()
+    {
+        currentWave++;
+
+        if (currentWave >= waves.Count)
+        {
+            currentWave = waves.Count - 1;
+        }
+
+        waveCounter = waves[currentWave].waveLength;
+        spawnCounter = waves[currentWave].timeBetweenSpawns;
+    }
+}
+
+[System.Serializable]
+public class WaveInfo
+{
+    public GameObject enemyToSpawn;
+    public float waveLength = 10f;
+    public float timeBetweenSpawns = 1f;
 }
